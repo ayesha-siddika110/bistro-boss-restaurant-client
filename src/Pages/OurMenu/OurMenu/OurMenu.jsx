5c255dffc8da74499a6398885d030b35ccae67df
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2025-01-03 10:57:07 +0600

    our menu page almost done

--- src/Pages/OurMenu/OurMenu/OurMenu.jsx
+++ src/Pages/OurMenu/OurMenu/OurMenu.jsx
@@ -3,6 +3,12 @@ import Banner from '../../../componants/Banner/Banner';
 import img from '../../../assets/assets/menu/banner3.jpg'
 import useMenu from '../../../Hooks/useMenu';
 import Heading from '../../../componants/Share/Heading/Heading';
+import OfferCard from '../../../componants/OfferCard/OfferCard';
+import MenuBanner from '../../../componants/MenuBanner/MenuBanner';
+import dessertimg from '../../../assets/assets/menu/dessert-bg.jpeg'
+import soupimg from '../../../assets/assets/menu/soup-bg.jpg'
+import pizzaimg from '../../../assets/assets/menu/pizza-bg.jpg'
+import saladimg from '../../../assets/assets/menu/salad-bg.jpg'
 
 const OurMenu = () => {
     const [menu, loading] = useMenu()
@@ -22,15 +28,60 @@ const OurMenu = () => {
             <Banner banner={img} heading={`OUR MENU`} subheading={`Would you like to try a dish?`}></Banner>
 
             <Heading   heading={`Todays offer`} subHeading={`Don't miss out`}></Heading>
-            
-
-            {/* conditional loading */}
+            <div className='flex justify-center items-center py-11'>
             {
              loading && <div className='flex justify-end items-center'>
              <span className="loading loading-spinner loading-lg "></span>
             
              </div>
             }
+            </div>
+
+            <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-8 w-[80%] m-auto'>
+                {
+                    offered?.map((item,index)=><OfferCard key={index} item={item}></OfferCard>)
+                }
+            </div>
+
+            {/* dessert  */}
+            
+            <MenuBanner banner={dessertimg} heading={`Dessert`} subheading={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eaque, possimus quo nemo accusantium odit, necessitatibus, repellendus expedita nisi reprehenderit ipsa. A consequuntur expedita ratione`}></MenuBanner>
+            <Heading   heading={`Desserts`} subHeading={`Don't miss out`}></Heading>
+            <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-8 w-[80%] m-auto'>
+                {
+                    dessert?.map((item,index)=><OfferCard key={index}  item={item}></OfferCard>)
+                }
+            </div>
+            {/* pizza  */}
+            
+            <MenuBanner banner={pizzaimg} heading={`Pizza`} subheading={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eaque, possimus quo nemo accusantium odit, necessitatibus, repellendus expedita nisi reprehenderit ipsa. A consequuntur expedita ratione`}></MenuBanner>
+            <Heading   heading={`Pizza's`} subHeading={`Don't miss out`}></Heading>
+            <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-8 w-[80%] m-auto'>
+                {
+                    pizza?.map((item,index)=><OfferCard key={index}  item={item}></OfferCard>)
+                }
+            </div>
+            {/* soup  */}
+            
+            <MenuBanner banner={soupimg} heading={`Soup`} subheading={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eaque, possimus quo nemo accusantium odit, necessitatibus, repellendus expedita nisi reprehenderit ipsa. A consequuntur expedita ratione`}></MenuBanner>
+            <Heading   heading={`Soup's`} subHeading={`Don't miss out`}></Heading>
+            <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-8 w-[80%] m-auto'>
+                {
+                    soup?.map((item,index)=><OfferCard key={index}  item={item}></OfferCard>)
+                }
+            </div>
+            {/* salad  */}
+            
+            <MenuBanner banner={saladimg} heading={`Salad`} subheading={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eaque, possimus quo nemo accusantium odit, necessitatibus, repellendus expedita nisi reprehenderit ipsa. A consequuntur expedita ratione`}></MenuBanner>
+            <Heading   heading={`salads`} subHeading={`Don't miss out`}></Heading>
+            <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-8 w-[80%] m-auto'>
+                {
+                    salad?.map((item,index)=><OfferCard key={index}  item={item}></OfferCard>)
+                }
+            </div>
+
+            {/* conditional loading */}
+            
         </div>
     );
 };
--- src/componants/OfferCard/OfferCard.jsx
+++ src/componants/OfferCard/OfferCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 // ----------------------------😑 offercard is working
 const OfferCard = ({item}) => {
     return (
-        <div key={index} className='flex'>
+        <div className='flex w-full border p-2 shadow-2xl'>
 
             <img src={item?.image} style={{ borderRadius: '0px 25px 20px 25px' }} className='w-16 h-16 object-cover' alt="img" />
-            <div className='pl-2'>
-                <div className='flex justify-between items-center'>
+            <div className='pl-2 w-full'>
+                <div className='flex justify-between '>
                     <h1 className='uppercase text-lg'>{item?.name}</h1>
                     <p className='text-lg font-semibold text-orange-600'>{item?.price}</p>
                 </div>
